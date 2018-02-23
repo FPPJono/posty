65a9151eeef484252cf02096f7ff7f53b14e7dcf
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-24 09:44:13 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ bot.on('ready', () => {
     console.log('I am ready!');
     bot.user.setPresence({ game: { name: 'somethin', type: 0 } }); //playing game
     var welcome = bot.channels.find("name", "welcome");
-    welcome.send("Welcome to the Swag Pigs Server!\nBy clicking the :white_check_mark: button below, you agree to all the rules stated in #rules.\n Once you have hit the checkmark, go ahead to #banter to say hi to everyone, and check out the other channel topics we have on the server! :pig:")
+    welcome.send("Welcome to the Swag Pigs Server!\nBy clicking the :white_check_mark: button below, you agree to all the rules stated in <#269998962717491201>.\nOnce you have hit the checkmark, go ahead to <#269990219665637377> to say hi to everyone, and check out the other channel topics we have on the server! :pig:")
 });
 
 bot.on('message', message => {
