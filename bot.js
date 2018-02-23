d7a7f8701057af3c3dc0bb238608709574937655
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-24 10:30:52 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,24 @@ bot.on('message', message => {
         }else
             message.author.send("sorry, that command is for admins only")
     }
+    if (message.content.startsWith(PREFIX + "resetWelcome")) {
+         if (message.member.roles.has("269993616456417280")) {
+                var welcome = bot.channels.find("name", "welcome");
+                async function clear() {
+                    msg.delete();
+                    const fetched = await msg.channel.fetchMessages({limit: 99});
+                    msg.channel.bulkDelete(fetched);
+                }
+                clear();
+                    welcome.send("Welcome to the Swag Pigs Server!\nBy clicking the :white_check_mark: button below, you agree to all the rules stated in <#269998962717491201>.\nOnce you have hit the checkmark, go ahead to <#269990219665637377> to say hi to everyone, and check out the other channel topics we have on the server! :pig:")
+                        .then(function (message) {
+                        message.react("✅")
+                    }).catch(function() {
+                        //Something
+                        });
+        }else
+            message.author.send("sorry, that command is for admins only")
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
