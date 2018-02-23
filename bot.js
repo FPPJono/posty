4e09467d093696cec8f65bdd1b94b1e200c3ca59
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-24 09:00:48 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,40 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const PREFIX = "!";
+
 
 bot.on('ready', () => {
     console.log('I am ready!');
+    bot.user.setPresence({ game: { name: 'somethin', type: 0 } }); //playing game
 });
 
 bot.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    if (message.content.startsWith(PREFIX + "announce")) {
+         if (message.member.roles.has("269993616456417280")) {
+            let content = args.join(" ")
+            var useContent = content.substr(9);
+            announcement.send(useContent)
+        }else
+            message.author.send("sorry, that command is for admins only")
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
 bot.login(process.env.BOT_TOKEN);
+
+
+//bot.on('message', message => {
+  //  var sender = message.author;
+    
+    //allows custom commands
+    //var args = message.content.substring(PREFIX.length).split(" ");
+    //var announcement = bot.channels.find("name", "announcements");
+    
+
+//bot.login(process.env.BOT_TOKEN);
+
+    
+    //member.addRole(member.guild.roles.find("name", "INSERTNAMEOFROLE"))
