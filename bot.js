4d4f478090b0ca037a1ecd80a5968fa33b240e69
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-26 21:24:52 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
     if(reaction.emoji.name === "✅") {
         const guildMember = reaction.users;
         console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
-        user.addRole('416075320190566400');
+        guildMember.addRole('416075320190566400');
     }
 });
 
