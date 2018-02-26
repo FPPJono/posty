f892ab8b67dbb4d97e0d571a13e22fe6e2983f5f
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-26 20:32:41 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const PREFIX = "!";
 
 bot.on('ready', () => {
     console.log('I am ready!');
-    bot.user.setPresence({ game: { name: 'you sleep', type: 0 } }); //playing game
+    bot.user.setActivity('FriendCodes', {type: "WATCHING"});//({ game: { name: 'you sleep', type: 0 } }); //playing game
 });
 
 bot.on('message', message => {
