1ebd8349cb7a0b79693d284ef39ef5ff68a3deaf
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-26 19:11:46 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,10 @@ bot.on('message', message => {
             .then(function (message) {
         message.react("✅")
                 });
+        if(reaction.emoji.name === "✅") {
+            console.log(reaction.users);
+            reaction.users.addRole("Piglet");
+        }
     }
     if (message.content.includes("<@416446498264580096>")) {
         message.channel.send("shut up");
