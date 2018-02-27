8e0baa623f8f448bba9db7c2ac7d0a0880ba5f2e
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-28 09:52:38 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,8 +80,8 @@ bot.on('message', message => {
         message.react('380221447295205376')
     }
     if (rip.content.includes("spreadsheet")) {
-        message.react('416071297920008192')
-        message.channel.send("ha loser")
+        rip.react('416071297920008192')
+        rip.channel.send("ha loser")
     }
     //if (message.content.startsWith(PREFIX + "piglet")) {
       //  let role = message.guild.roles.find("name", "Piglet");
