8fe0f9f175af771a3b80cf55e2ea35cb3386db75
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-27 18:29:39 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const PREFIX = "!";
 bot.on('ready', () => {
     console.log('I am ready!');
     bot.user.setPresence({ game: { name: 'I turned on !!', type: 0 } }); //playing game
-    window.setTimeout(slowAlert, 5000);
+    scope.setTimeout(slowAlert, 5000);
     bot.user.setPresence({ game: { name: "in some dirt", type: 0}});
 });
 
