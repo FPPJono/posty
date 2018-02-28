307f71d9016c9230ac3b51f7fd89dfec1492ac00
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-02-28 19:04:15 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ bot.on("message", async message => {
         message.channel.bulkDelete(fetched)
             .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
     }
-    if(message.content.startsWith(PREFIX + "say") {
+    if(message.content.startsWith(PREFIX + "say")) {
         const sayMessage = args.join(" ");
         message.delete().catch(O_o=>{}); 
         message.channel.send(sayMessage);
