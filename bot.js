d04c0b5c1ae39ae42a669a0ec9df0a5d9940425d
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-03-07 20:23:14 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,7 +74,8 @@ bot.on('message', message => {
             announcement.send(useContent)
             console.log(`${sender.username} just announced ${useContent}.`)
         }else
-            message.author.send("sorry, that command is for admins only")
+            message.channel.send("sorry, that command is for admins only")
+                .then(m => m.delete(5000));
     }
     if (message.content.startsWith(PREFIX + "playing")) {
          if (message.member.roles.has("269993616456417280")) {
@@ -83,15 +84,20 @@ bot.on('message', message => {
             bot.user.setPresence({ game: { name: useContent, type: 0 } });
             console.log(`${sender.username} just changed the game to ${useContent}`)
         }else
-            message.author.send("sorry, that command is for admins only")
+            message.channel.send("sorry, that command is for admins only")
+                .then(m => m.delete(5000));
     }
     if (message.content.startsWith(PREFIX + "welcome")) {
-        var welcome = bot.channels.find("name", "welcome");
-        welcome.bulkDelete(99)
-        welcome.send("Welcome to the Swag Pigs Server!\nBy clicking the ✅ button below, you agree to all the rules stated in <#269998962717491201>.\nOnce you have hit the checkmark, go ahead to <#269990219665637377> to say hi to everyone, and check out the other channel topics we have on the server! 🐷")
-            .then(function (message) {
-        message.react("✅")
-                });
+        if message.member.roles.has("269993616456417280") {
+            var welcome = bot.channels.find("name", "welcome");
+            welcome.bulkDelete(99)
+            welcome.send("Welcome to the Swag Pigs Server!\nBy clicking the ✅ button below, you agree to all the rules stated in <#269998962717491201>.\nOnce you have hit the checkmark, go ahead to <#269990219665637377> to say hi to everyone, and check out the other channel topics we have on the server! 🐷")
+                .then(function (message) {
+            message.react("✅")
+                    });
+        }else
+            message.channel.send("sorry, that command is for admins only")
+                .then(m => m.delete(5000));
     }
     if (rip.includes("<@416446498264580096>")) {
         message.channel.send("shut up");
@@ -155,7 +161,8 @@ bot.on('message', message => {
                         .then(m => m.delete(5000));
             })
         }else
-            message.channel.send("sorry thats for admins only :/");
+            message.channel.send("sorry thats for admins only :/")
+                .then(m => m.delete(5000));
     }
     if (message.content.startsWith(PREFIX + "8ball")) {
         if (args[1] != null){
@@ -173,21 +180,5 @@ bot.on('message', message => {
     }
 });
 
-
-
-// THIS  MUST  BE  THIS  WAY
+// Sneaky Sneaky Token. Dont Share Kiddos
 bot.login(process.env.BOT_TOKEN);
-
-
-//bot.on('message', message => {
-  //  var sender = message.author;
-    
-    //allows custom commands
-    //var args = message.content.substring(PREFIX.length).split(" ");
-    //var announcement = bot.channels.find("name", "announcements");
-    
-
-//bot.login(process.env.BOT_TOKEN);
-
-    
-    //member.addRole(member.guild.roles.find("name", "INSERTNAMEOFROLE"))
