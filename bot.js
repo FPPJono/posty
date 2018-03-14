3dd3d8d11342b1d20400a945984bc10997e678c8
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-03-15 07:50:29 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,12 +86,12 @@ bot.on('message', message => {
         member.send("h")
     }
     if (message.channel.id === artChannel) {
-        if (message.attatchments.size >= 1) {
+        if (message.attatchments >= 1) {
             message.react('❤')
         }
     }
     if (message.channel.id === memesChannel) {
-        if (message.attatchments.size >= 1) {
+        if (message.attatchments >= 1) {
             message.react('👌')
             message.react('👎')
         }
