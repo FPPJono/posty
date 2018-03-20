2ecaa9b90d0c194982984ec457d596cfe47b6771
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-03-20 21:10:22 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -236,7 +236,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
         }
         bot.channels.find("name","general").send(`Welcome ${reaction.users.array().toString().substr(reaction.users.array().toString().length - 21)} to the Swag Pigs server!`);
         console.log(`${reaction.users.array().toString()} reacted with "${reaction.emoji.name}".`);
-        member.addRole('416075320190566400');
+        member.addRole('421804369789452288');
     }
 });
 
