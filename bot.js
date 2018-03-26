3a3e0a46f1021c48d03f7933e8d365d0bca11840
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-03-26 20:41:16 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,20 @@ var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
-
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('./client_secret.json');
+ 
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+var doc = new GoogleSpreadsheet('_vjNCPU8lsbbEFfGBWPIE9AovE_Df1eX4xORNxLK5g');
+ 
+// Authenticate with the Google Spreadsheets API.
+doc.useServiceAccountAuth(creds, function (err) {
+ 
+  // Get all of the rows from the spreadsheet.
+  doc.getRows(1, function (err, rows) {
+    console.log(rows);
+  });
+});
 
 //Bot Code
 
