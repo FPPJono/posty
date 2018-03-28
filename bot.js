0c5d028f2b8aedb112ff0b2e429484ed8976ae34
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-03-28 19:17:53 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -255,8 +255,7 @@ bot.on('message', message => {
         guild.channels.get(suggestChannel).send({ embed });
     }   
     if (message.content.startsWith(PREFIX + "avatar")) {
-        if (message.mentions.users.array().toString().length <= 0) {
-            message.author.send(message.mentions.users.array().toString())
+        if (message.mentions.users.array().toString().length <= 1) {
             message.channel.send({files: ["https://cdn.discordapp.com/attachments/428427184877600769/428427196516925442/IMG_20180322_124222_038.jpg"]})
         } else message.channel.send({files:[message.author.avatarURL]})
     }
