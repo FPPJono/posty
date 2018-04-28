2560a8466934220e7672f7f631ee5bd8db777ba9
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-04-28 12:44:49 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ bot.on('message', message => {
         }
     }
     const swearWords = ["nigger", "chink", "tranny", "fag", "dyke", "nigga", "kike", "retard", "autist", "negroid", "dike"];
-    var swearCheck = rip.replace(/\s/g/U+200B, '')
+    var swearCheck = rip.replace(/\s/g/U200B, '')
     const byPass = ["halfaglass", "klondike"]
     if( swearWords.some(word => swearCheck.includes(word))) {
         if (byPass.some(word => swearCheck.includes(word))) return;
