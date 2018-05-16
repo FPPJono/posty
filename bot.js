ee881e58d2dba460f8993766e40b0dd12e57ffcb
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-05-16 18:41:01 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -191,18 +191,13 @@ bot.on('message', message => {
             const sayMessage = args.join(" ");
             var useContent = sayMessage.substr(5);
             var attachments = (message.attachments).array()
-            message.delete().catch(O_o => { });
-            message.channel.startTyping()
-                .then(() => {
-                    wait((useContent.length/3)*1000)
-                    if (message.attachments.array().length >= 1) {
-                        message.channel.send(`${useContent}`)
-                        attachments.forEach(function (attachment) { message.channel.send({ file: `${attachment.url}` }) })
-                    }
-                    if (message.attachments.array().length <= 0) { message.channel.send(`${useContent}`) }
-                    message.channel.stopTyping()
-                })
-
+            message.delete().catch(O_o => { })
+            if (message.attachments.array().length >= 1) {
+                message.channel.send(`${useContent}`)
+                attachments.forEach(function (attachment) { message.channel.send({ file: `${attachment.url}` }) })
+            }
+            if (message.attachments.array().length <= 0) { message.channel.send(`${useContent}`) }
+            message.channel.stopTyping()
         } else
             message.channel.send("sorry thats for admins only");
     }
