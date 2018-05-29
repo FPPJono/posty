04a101b52000fd4ca3855bc7306e4aa6c9349bd5
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-05-29 21:05:45 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ bot.on('message', message => {
                 0, 0, 500, 500               // destination dimensions
             );
             c.font = "24pt, 'Score Font'"
-            c.fillText("befpacito", 80, 80)
+            c.fillText(text: "befpacito", x: 10, y:80)
             PImage.encodeJPEGToStream(img2,fs.createWriteStream('scorecards/score.jpg')).then(() => {
                 console.log(`${message.author.username} has just checked their score`);
                 message.channel.send({files:[{attachment: 'scorecards/score.jpg', name:'score.jpg'}] })
