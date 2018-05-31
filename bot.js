f7b8c4aa8f0d57b71994a19b6408359a8394c7e2
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-05-31 16:12:11 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,7 +138,7 @@ bot.on("message", async message => {
             var ctx = c
             var fnt = PImage.registerFont('scorefont.ttf', 'Score Font')
             fnt.load(() => {
-                ctx.fillStyle = #ffffff;
+                ctx.fillStyle = '#ffffff';
                 ctx.font = "20pt 'Score Font'";
                 ctx.fillText(`${person.username}`, 148, 158);
                 ctx.font = "20pt 'Score Font'";
