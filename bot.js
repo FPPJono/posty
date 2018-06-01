9337dd7899fe9a718788e7a55843a64dbb0fe4f9
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-06-01 15:52:54 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -147,7 +147,7 @@ function customRole(message, color, name, x) {
                 ctx.fillText("ROLE", 190, 200)
                 ctx.font = "30pt 'Score Font'"
                 ctx.fillText(`${name.toUpperCase()}`, x, 148);
-                ctx.fillRect(x,153,(x + (name.length()*40)),156);
+                ctx.fillRect(x,153,(x + (name.toString().length * 40)),156);
                 PImage.encodePNGToStream(img2,fs.createWriteStream('scorecards/role.png')).then(() => {
                     message.channel.send({files:[{attachment: 'scorecards/role.png', name:'role.png'}] })
                 });
