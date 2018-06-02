3d602b30dc1410d484adef8ca54142efe3ca6f84
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-06-02 14:04:59 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,16 @@ const beerbongs = '450155434356113418'
 const stoney = '450155315095142413'
 const august26 = '450155482263584768'
 
+//Role Tiers
+const fans = '452239200444743700' //Tier 1
+const noticeMe = '452232318921211904' //Tier 2
+const rockstar = '452232317390290955' //Tier 3
+const goFlex = '452232316295708673' //Tier 4
+const hollywoodDreamers = '452232315653980171' //Tier 5
+
+//Song Roles
+
+
 //lists
 var eightBall = ["I would say..... yes!", "Probably not", "heck maybe, idk", "I dont think so", "eh, probably", "hmmm.... maybe not", "*concentrate*, and try again", "look man im just a bot go ask someone who cares", "those who ask will get their answer eventually, try again", "haha! yes!", "hah, nope"]
 var coinFlip = ["The coin landed on heads!", "The coin landed on tails"]
@@ -168,7 +178,8 @@ bot.on("guildMemberAdd", async member => {
     let guild = member.guild;
     welcomecard(member.user, guild)
     let RoleMember = guild.member(member.user);
-    RoleMember.addRole(beerbongs);
+    RoleMember.addRole(beerbongs)
+    RoleMember.addRole(fans)
 });
 
 bot.on("message", async message => {
@@ -182,7 +193,7 @@ bot.on("message", async message => {
     } else {
         var person = message.author
     }
-    if (message.content.startsWith('!score')) { 
+    if (rip.startsWith('!score')) { 
         if (person.id === '246840305741987840') {
             await message.channel.send('toot')
         }
@@ -207,7 +218,7 @@ bot.on('message', message => {
     const args = message.content.split(" ");
     let rip = message.content.toLowerCase()
     let guild = message.guild
-    if (message.content.startsWith(PREFIX + "ping")) {
+    if (rip.startsWith(PREFIX + "ping")) {
         message.channel.send(`Pong! ${new Date().getTime() - message.createdTimestamp}ms`)
     }
     if (rip.startsWith('!role')) {
@@ -278,13 +289,13 @@ bot.on('message', message => {
             message.react('👎')
         }
     }
-    if (message.content.startsWith(PREFIX + "randomhex")) {
+    if (rip.startsWith(PREFIX + "randomhex")) {
         let color = getRandomInt(16777215)
         var embed = basicEmbed(color, `#${decimalToHexString(color)}`)
         message.channel.send({ embed });
     }
 
-    if (message.content.startsWith(PREFIX + "send")) {
+    if (rip.startsWith(PREFIX + "send")) {
         if (message.member.roles.has(admin)) {
             const sayMessage = args.join(" ");
             var useContent = sayMessage.substr(5);
@@ -298,13 +309,13 @@ bot.on('message', message => {
         } else
             message.channel.send("sorry thats for staff only");
     }
-    if (message.content.startsWith(PREFIX + "rate")) {
+    if (rip.startsWith(PREFIX + "rate")) {
         const thingToRate = args.join(" ");
         var ratedThing = thingToRate.substr(5);
         var embed = basicEmbed(65535, `I would rate ${ratedThing} ${getRandomInt(10)} out of 10!`)
         message.channel.send({ embed });
     }
-    if (message.content.startsWith(PREFIX + "clear")) {
+    if (rip.startsWith(PREFIX + "clear")) {
         if (message.member.roles.has(admin)) {
             message.delete()
             let messagecount = parseInt(args[1]) || 1;
@@ -321,24 +332,24 @@ bot.on('message', message => {
             message.channel.send("sorry thats for staff only :/")
                 .then(m => m.delete(5000));
     }
-    if (message.content.startsWith(PREFIX + "8ball")) {
+    if (rip.startsWith(PREFIX + "8ball")) {
         if (args[1] != null) {
             var embed = basicEmbed(122353, `${eightBall[Math.floor(Math.random() * eightBall.length).toString(16)]}`)
             message.channel.send({ embed });
         } else message.channel.send("where is the question? \n```Correct usage: !8ball question```");
     }
-    if (message.content.startsWith(PREFIX + "coinflip")) {
+    if (rip.startsWith(PREFIX + "coinflip")) {
         var embed = basicEmbed(16776448, `${coinFlip[Math.floor(Math.random() * coinFlip.length).toString(16)]}`)
         message.channel.send({ embed });
     }
-    if (message.content.startsWith(PREFIX + "warn")) {
+    if (rip.startsWith(PREFIX + "warn")) {
         if (message.member.roles.has(admin)) {
             let guild = message.guild;
             let warning = message.content.substr(28)
             guild.member(message.mentions.users.first()).send(`you have been warned for: \`${warning}\` Please improve your behaviour or you may be kicked or banned from this server in the future.`)
         } else message.channel.send("sorry that command is for staff only");
     }
-    if (message.content.startsWith(PREFIX + "userinfo")) {
+    if (rip.startsWith(PREFIX + "userinfo")) {
         let guild = message.guild;
         if (message.mentions.users.array().toString().length >= 1) {
             var person = message.mentions.users.first()
@@ -380,7 +391,7 @@ bot.on('message', message => {
         };
         message.channel.send({ embed });
     }
-    if (message.content.startsWith(PREFIX + "suggest")) {
+    if (rip.startsWith(PREFIX + "suggest")) {
         let guild = message.guild;
         let suggestion = message.content.substr(8)
         let color = message.guild.member(message.author).displayColor
@@ -400,7 +411,7 @@ bot.on('message', message => {
         };
         guild.channels.get(suggestChannel).send({ embed });
     }
-    if (message.content.startsWith(PREFIX + "avatar")) {
+    if (rip.startsWith(PREFIX + "avatar")) {
         if (message.mentions.users.array().toString().length >= 1) {
             var pfp = message.mentions.users.first().avatarURL
             message.channel.send({ files: [{ attachment: pfp, name: `avatar${pfp.slice(0, -10).substr(pfp.slice(0, -10).length - 4)}` }] })
@@ -409,13 +420,13 @@ bot.on('message', message => {
             message.channel.send({ files: [{ attachment: pfp, name: `avatar${pfp.slice(0, -10).substr(pfp.slice(0, -10).length - 4)}` }] })
         }
     }
-    if (message.content.startsWith(PREFIX + "morse")) {
+    if (rip.startsWith(PREFIX + "morse")) {
         var chars = { ' ': '/', 'a': '.- ', 'b': '-... ', 'c': '-.-. ', 'd': '-.. ', 'e': '. ', 'f': '..-. ', 'g': '--. ', 'h': '.... ', 'i': '.. ', 'j': '.--- ', 'k': '-.- ', 'l': '.-.. ', 'm': '-- ', 'n': '-. ', 'o': '--- ', 'p': '.--. ', 'q': '--.- ', 'r': '.-. ', 's': '... ', 't': '- ', 'u': '..- ', 'v': '...- ', 'w': '.-- ', 'x': '-..- ', 'y': '-.-- ', 'z': '--.. ', '1': '.---- ', '2': '..--- ', '3': '...-- ', '4': '....- ', '5': '..... ', '6': '-.... ', '7': '--... ', '8': '---.. ', '9': '----. ', '0': '----- ' };
         var s = rip.substr(7);
         s = s.replace(/[abcdefghijklmnopqrstuvwxyz1234567890 ]/g, m => chars[m]);
         message.channel.send(`${s}`)
     }
-    if (message.content.startsWith(PREFIX + "emote")) {
+    if (rip.startsWith(PREFIX + "emote")) {
         var chars = { ' ': '⬜', 'a': '🅰 ', 'b': '🅱 ', 'c': '🇨 ', 'd': '🇩 ', 'e': '🇪 ', 'f': '🇫 ', 'g': '🇬 ', 'h': '🇭 ', 'i': '🇮 ', 'j': '🇯 ', 'k': '🇰 ', 'l': '🇱 ', 'm': '🇲 ', 'n': '🇳 ', 'o': '🅾 ', 'p': '🇵 ', 'q': '🇶 ', 'r': '🇷 ', 's': '🇸 ', 't': '🇹 ', 'u': '🇺 ', 'v': '🇻 ', 'w': '🇼 ', 'x': '🇽 ', 'y': '🇾 ', 'z': '🇿 ' };
         var s = rip.substr(7);
         s = s.replace(/[abcdefghijklmnopqrstuvwxyz ]/g, m => chars[m]);
