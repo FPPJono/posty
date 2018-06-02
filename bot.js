0296a2a8a446b9470a7c802dc7958dd05e8032a2
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-06-03 08:21:55 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,6 +96,7 @@ async function scorecard(role, color, person, message, data) {
                 ctx.font = "35pt 'Score Font'"
                 ctx.fillText(userScore.score, 14, 221)
                 ctx.fillText(userScore.credits, 14, 365)
+                ctx.fillText(userScore.level, 14, 292)
                 PImage.decodePNGFromStream(fs.createReadStream(`scorecards/pfp.png`)).then((pfp) => {
                     c.drawImage(pfp,
                         0, 0, pfp.width, pfp.height,
