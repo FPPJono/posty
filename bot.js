a66b828c0c5caaaf73a70203a6853d117346e1f2
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-06-03 07:06:56 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,7 +79,7 @@ async function scorecard(role, color, person, message, data) {
         })
     }
     var userData = data.read({search:{userid:person.id}})
-    console.log(userData.toString())
+    console.log(userData)
     PImage.decodePNGFromStream(fs.createReadStream(`scorecards/${role}.png`)).then((img) => {
         var img2 = PImage.make(500,500);
         var c = img2.getContext('2d');
