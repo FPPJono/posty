7236b7bdf6ff0507b82a46dc77cede07dfb51ea6
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-06-04 10:32:59 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,6 +223,22 @@ bot.on("message", async message => {
     } else {
         var person = message.author
     }
+    if (rip.startsWith('!help')) {
+        switch (rip.substr(6)) {
+            case 'random':
+                message.channel.send({basicEmbed("!randomhex *sends a random color*\n!rate [thing] *rates a thing*\n!coinflip *flips a coin!*\n!8ball [question] *uses a magic 8ball*", getRandomInt(16777215))})
+            case 'converting':
+                message.channel.send({basicEmbed("!morse *converts text to morse code*\n!emote *converts text to emotes*", getRandomInt(16777215))})
+            case 'staff':
+                message.channel.send({basicEmbed("!send [message] *sends a message*\n!warn @person [warning] *sends warning to member*\n!playing [thing] *sets playing status*\n!watching [thing] *sets watching status*\n!clear [number from 2-100] *deletes a certain number of messages*",getRandomInt(16777215))})
+            case 'info':
+                message.channel.send({basicEmbed("!ping *pings the bot*\n!userinfo (@person) *gets info about yourself or another member*\n!avatar *sends current profile pic of you or person mentioned*\n!suggest [suggestion] *sends a suggestion to a staff channel*",getRandomInt(16777215))})
+            case 'user':
+                message.channel.send({basicEmbed("!role [album] *gives album role*\n!score *sends score of person. WIP*",getRandomInt(16777215))})
+            default:
+                message.channel.send({basicEmbed("Command Sections\nRandom\nConverting\nStaff\nInfo\nUser",getRandomInt(16777215))})
+        }
+    }
     if (rip.startsWith('!score')) { 
         if (person.id === '246840305741987840') {
             await message.channel.send('sucky wucky 😏')
