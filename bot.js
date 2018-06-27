9cf5c5e5855b2d57ddb02477cb89e7a9bf30f9ae
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-06-27 20:07:29 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ var gifFrames = require('gif-frames')
 var gameMessage = new Function('return true')
 const download = require('image-downloader')
 const sheetsu = require('sheetsu-node')
+const GifCreationService = require('gif-creation-service')
 
 var PImage = require('pureimage');
 var img1 = PImage.make(500,500);
@@ -85,13 +86,65 @@ function decimalToHexString(number) {
     return number.toString(16).toUpperCase();
 }
 
+
+
+async function gifScore(role, color, person, message, height, name) {
+    var size = (600 / name.length)
+    if (size > 50){
+        size = 50
+    }
+    var frames = []
+    var i = 0
+    while(i < 5) {
+        await gifFrames({url:person.displayAvatarURL, frames:i, outputType: 'png'}).then(function(frameData){
+            frameData[i].getImage().pipe(fs.createWriteStream(`scorecards/pfp.png`))
+        })
+        PImage.decodePNGFromStream(fs.createReadStream(`scorecards/${role}.png`)).then((img) => {
+            var img2 = PImage.make(500,500);
+            var c = img2.getContext('2d');
+            c.drawImage(img,
+                0, 0, img.width, img.height, // source dimensions
+                0, 0, 500, 500               // destination dimensions
+            );
+            var ctx = c
+            var fnt = PImage.registerFont('scorefont.ttf', 'Score Font')
+            fnt.load(() => {
+                ctx.fillStyle = color;
+                ctx.font = `${size}pt 'Score Font'`;
+                ctx.fillText(`${name.toUpperCase()}`, 135, 80);
+                ctx.font = "35pt 'Score Font'"
+                ctx.fillText("this", 14, 221)
+                ctx.fillText("currently", 14, 292)
+                ctx.fillText("doesn't", 14, 365)
+                ctx.fillText("work", 14, 435)
+                ctx.fillRect(340, height, 150, 150)
+                PImage.decodePNGFromStream(fs.createReadStream(`scorecards/pfp.png`)).then((pfp) => {
+                    c.drawImage(pfp,
+                        0, 0, pfp.width, pfp.height,
+                        15, 15, 110, 110
+                   )
+                    PImage.encodePNGToStream(img2,fs.createWriteStream(`scorecards/score${i}.png`)).then(() => {
+                        console.log(`frame ${i + 1} of ${name}'s score has been made`);
+                        frames.push(`scorecards/score${i}`)
+                    });
+                })
+            });
+        });
+    }
+    const outputGifFile = 'scorecards/score.gif'
+    GifCreationService.createAnimatedGifFromPngImages(frames, outputGifFile, {repeat:true, fps:10, quality:1})
+    .then(outputGifFile = > {
+        console.log(`${person.username}'s score has just been checked, id:${person.id}`)
+    }
+    message.channel.send({files:[{attachment: 'scorecards/score.gif', name:'score.gif'}] })
+}
+
 async function scorecard(role, color, person, message, height, name) {
     if ((person.displayAvatarURL.includes("png"))||(person.displayAvatarURL.includes("jpg"))){
         await download.image({url: person.displayAvatarURL, dest:`scorecards/pfp.png`})
     }else if(person.displayAvatarURL.includes("gif")){
-        await gifFrames({url:person.displayAvatarURL, frames:0, outputType: 'png'}).then(function(frameData){
-            frameData[0].getImage().pipe(fs.createWriteStream(`scorecards/pfp.png`))
-        })
+        gifScore(role,color,person,message,height,name)
+        return;
     }
     var size = (600 / name.length)
     if (size > 50){
