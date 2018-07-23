2a4780ca975e33a6e826fdd614a4752f5eb6bbb0
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-07-23 19:51:25 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,9 +93,8 @@ async function gifScore(role, color, person, message, height, name) {
     }
     var frames = []
     var i = 0
-    var frameCount = 0
     await gifFrames({url:person.displayAvatarURL, frames:'all', outputType: 'png'}).then(function(frameData){
-        frameData.length = frameCount
+        var frameCount = frameData.length
         console.log(`users pfp contains ${frameCount} frames yehaw`)
     })
     console.log(`users pfp contains ${frameCount} frames`)
