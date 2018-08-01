301db2dae3e33ad534481b884c5fe0dec1b692f8
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-08-01 21:32:36 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -310,20 +310,24 @@ bot.on("message", async message => {
         } else message.channel.send("please set a hex value for the image to be tinted to \n``Correct Usage: !tint #6 character hex value (@person)``")
         if ((person.displayAvatarURL.includes("png"))||(person.displayAvatarURL.includes("jpg"))){
             await download.image({url: person.displayAvatarURL, dest:`pfp.png`})
+            Jimp.read("pfp.png").then(function (image) {
+                image.greyscale()
+                     .write("tint.png")
+            }).catch(function (err) {
+                console.error(err);
+            });
         }else if(person.displayAvatarURL.includes("gif")){
             await gifFrames({url:person.displayAvatarURL, frames:0, outputType: 'png'}).then(function(frameData){
                 frameData[0].getImage().pipe(fs.createWriteStream(`pfp.png`))
+                Jimp.read("pfp.png").then(function (image) {
+                    image.greyscale()
+                         .write("tint.png")
+                }).catch(function (err) {
+                    console.error(err);
+                });
             })
         }
-        Jimp.read("pfp.png").then(function (image) {
-            image.resize(256, 256)            // resize
-                 .quality(60)                 // set JPEG quality
-                 .greyscale()                 // set greyscale
-                 .write("tint.png"); // save
-        }).catch(function (err) {
-            console.error(err);
-        });
-        message.channel.send("noodle", {files:[{attachment: 'tint.png', name:'tint.png'}] })
+        message.channel.send({files:[{attachment: 'tint.png', name:'tint.png'}] })
     }
     if (rip.startsWith('!help')) {
         if (rip.substr(6).startsWith('random')){
