08975673de604f484ef2f72f04c1de225c510e85
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-09-08 13:49:22 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -314,22 +314,14 @@ bot.on("message", async message => {
     var swearCheck = message.content.toLowerCase().replace(/halfaglass/g,"").replace(/klondike/g,"").replace(/warfage/g,"").replace(/of a g/g, "").replace(/f ago/g, "").replace(/fire retardant/g, "").replace(/\s+/g, '');
     if (swearWords.some(word => swearCheck.includes(word))) {
         var slursFound = []
-        if (swearCheck.includes("nigger")) slursFound.push("nigger")
-        if (swearCheck.includes("negro")) slursFound.push("negro")
-        if (swearCheck.includes("chink")) slursFound.push("chink")
-        if (swearCheck.includes("nigga")) slursFound.push("nigga")
-        if (swearCheck.includes("tranny")) slursFound.push("tranny")
-        if (swearCheck.includes("fag")) slursFound.push("fag")
-        if ((swearCheck.includes("dyke"))||(swearCheck.includes("dike"))) slursFound.push("dike")
-        if (swearCheck.includes("kike")) slursFound.push("kike")
-        if (swearCheck.includes("retard")) slursFound.push("retard")
-        if (swearCheck.includes("negroid")) slursFound.push("negroid")
-        let guild = message.guild;
-        let color = message.guild.member(message.author).displayColor
+        var i = 0
+        for (i in swearWords) {
+            if (swearCheck.includes(swearWords[i])) slursFound.push(swearWords[i])
+            i = i+1
+        }
         message.channel.send("Please refrain from using slurs. A copy of your message has been sent to the Admins.")
-            .then(m => m.delete(7500));
-        var embed = pfpEmbed(color, ["Message sent in channel:", "Message content:", "User ID:", "Slurs found:"], [`<#${message.channel.id}>`,`${message.content.substr(0, 1024)}`,`${message.author.id}`,`${slursFound.toString().replace(/,/g,", ")}`], `${message.author.username} has been detected using slurs`, `${message.author.avatarURL}`)
-        guild.channels.get(slurChannel).send({ embed });
+        var embed = pfpEmbed(message.guild.member(message.author).displayColor, ["Message sent in channel:", "Message content:", "User ID:", "Slurs found:"], [`<#${message.channel.id}>`,`${message.content.substr(0, 1024)}`,`${message.author.id}`,`${slursFound.toString().replace(/,/g,", ")}`], `${message.author.username} has been detected using slurs`, `${message.author.avatarURL}`)
+        message.guild.channels.get(slurChannel).send({ embed });
         return
     }
     if (message.mentions.users.array().toString().length >= 1) {
@@ -705,25 +697,17 @@ bot.on('messageUpdate', (omsg, nmsg) => {
     var embed = pfpEmbed(color, ["Channel", "Original Message Content", "New Message Content"], [`<#${omsg.channel.id}>`,`${omsg.content.substr(0, 1024)}`, `${nmsg.content.substr(0,1024)}`], `${omsg.author.username} just edited their message!`, `${omsg.author.avatarURL}`)
     guild.channels.get(deleteEditChannel).send({ embed });
     const swearWords = ["nigger", "chink", "tranny", "fag", "dyke", "nigga", "kike", "retard", "negroid", "dike", "negro"];
-    var swearCheck = nmsg.content.toLowerCase().replace(/halfaglass/g,"").replace(/klondike/g,"").replace(/warfage/g,"").replace(/of a g/g, "").replace(/f ago/g, "").replace(/fire retardant/g, "").replace(/\s+/g, '');
+    var swearCheck = nmsg.content.toLowerCase().replace(/snigger/g,"").replace(/halfaglass/g,"").replace(/klondike/g,"").replace(/warfage/g,"").replace(/of a g/g, "").replace(/f ago/g, "").replace(/fire retardant/g, "").replace(/\s+/g, '');
     if (swearWords.some(word => swearCheck.includes(word))) {
         var slursFound = []
-        if (swearCheck.includes("nigger")) slursFound.push("nigger")
-        if (swearCheck.includes("negro")) slursFound.push("negro")
-        if (swearCheck.includes("chink")) slursFound.push("chink")
-        if (swearCheck.includes("nigga")) slursFound.push("nigga")
-        if (swearCheck.includes("tranny")) slursFound.push("tranny")
-        if (swearCheck.includes("fag")) slursFound.push("fag")
-        if ((swearCheck.includes("dyke"))||(swearCheck.includes("dike"))) slursFound.push("dike")
-        if (swearCheck.includes("kike")) slursFound.push("kike")
-        if (swearCheck.includes("retard")) slursFound.push("retard")
-        if (swearCheck.includes("negroid")) slursFound.push("negroid")
-        let guild = nmsg.guild;
-        let color = nmsg.guild.member(nmsg.author).displayColor
+        var i = 0
+        for (i in swearWords) {
+            if (swearCheck.includes(swearWords[i])) slursFound.push(swearWords[i])
+            i = i+1
+        }
         nmsg.channel.send("Please refrain from using slurs. A copy of your message has been sent to the Admins.")
-            .then(m => m.delete(7500));
-        var embed = pfpEmbed(color, ["Message sent in channel:", "Message content:", "User ID:", "Slurs found:"], [`<#${nmsg.channel.id}>`,`${nmsg.content.substr(0, 1024)}`,`${nmsg.author.id}`,`${slursFound.toString().replace(/,/g,", ")}`], `${nmsg.author.username} has been detected using slurs`, `${nmsg.author.avatarURL}`)
-        guild.channels.get(slurChannel).send({ embed });
+        var embed = pfpEmbed(nmsg.guild.member(nmsg.author).displayColor, ["Message sent in channel:", "Message content:", "User ID:", "Slurs found:"], [`<#${nmsg.channel.id}>`,`${nmsg.content.substr(0, 1024)}`,`${nmsg.author.id}`,`${slursFound.toString().replace(/,/g,", ")}`], `${nmsg.author.username} has been detected using slurs`, `${nmsg.author.avatarURL}`)
+        nmsg.guild.channels.get(slurChannel).send({ embed });
         return
     }
 });
